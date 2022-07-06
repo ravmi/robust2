3cc9c7369a24f625306da16108be52f17292e3a2
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2022-07-06 12:34:00 +0200

    double ver

--- supervised.py
+++ supervised.py
@@ -11,37 +11,13 @@ from datasets import DenseGraspDataset, DenseGraspDataModule, DenseGraspDatasetR
 from pgd import get_adv_examples
 from steps import LinfStep, L2Step
 import torchvision.transforms as T
-
-
-
-def replace_best(loss, bloss, x, bx):
-    if bloss is None:
-        bx = x.clone().detach()
-        bloss = loss.clone().detach()
-    else:
-        replace = bloss < loss
-        bx[replace] = x[replace].clone().detach()
-        bloss[replace] = loss[replace]
-
-    return bloss, bx
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import random
+import yaml
+from deepaug import DeepAug
 
 
 class SupervisedDenseGrasp(pl.LightningModule):
-    def __init__(self, batch_size, lr, weight_decay, dropout, max_epochs, robust, iterations, eps, step_size, step_type, positive_loss_scaling=1.):
+    def __init__(self, batch_size, lr, weight_decay, dropout, max_epochs, robust, iterations, eps, step_size, step_type, use_deepaug, positive_loss_scaling=1.):
         super().__init__()
         self.save_hyperparameters()
         self.eps = eps
@@ -49,6 +25,9 @@ class SupervisedDenseGrasp(pl.LightningModule):
         self.iterations = iterations
         self.robust=robust
         self.step_type=step_type
+        self.use_deepaug = use_deepaug
+        if self.use_deepaug:
+            self.deepaug = DeepAug()
 
 
         self.img_log_step = 0
@@ -67,7 +46,7 @@ class SupervisedDenseGrasp(pl.LightningModule):
         optimizer = optim.AdamW(self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay)
         return optimizer
 
-    def logging(self, mode, batch, outputs, loss, top_n: Optional[int] = None):
+    def logging(self, mode, batch, outputs, loss, top_n: Optional[int] = None, dataloader_idx=0):
         img, labels, mask = batch
         labels = labels.reshape((labels.shape[0], -1))
         outputs = outputs.reshape((outputs.shape[0], -1))
@@ -80,7 +59,10 @@ class SupervisedDenseGrasp(pl.LightningModule):
         discretized_outputs = torch.as_tensor(torch.sigmoid(outputs) > 0.5, dtype=torch.long)
 
         def naming(label, top_n=top_n):
-            return f"{mode}_{label}" if top_n is None else f"{mode}_top{top_n}_{label}"
+            name = f"{mode}_{label}" if top_n is None else f"{mode}_top{top_n}_{label}"
+            name = f"{dataloader_idx}_{name}"
+
+            return name
 
         self.log(naming("loss", None), loss.mean(), on_step=True, on_epoch=True)
         self.log(naming("size", None), len(self.trainer.train_dataloader.dataset) if mode == "train" else len(
@@ -90,8 +72,8 @@ class SupervisedDenseGrasp(pl.LightningModule):
         if torch.sum(labels == 1).item() > 0:
             self.log(naming("recall"), (torch.sum(torch.logical_and(discretized_outputs == 1, labels == 1)) / torch.sum(
                 labels == 1)).item(), on_step=True, on_epoch=True)
-        self.log(naming("labels"), torch.sum(labels == 1).item(), on_step=True, on_epoch=True)
-        self.log(naming("detections"), torch.sum(discretized_outputs == 1).item(), on_step=True, on_epoch=True)
+        self.log(naming("labels"), torch.sum(labels == 1).float().item(), on_step=True, on_epoch=True)
+        self.log(naming("detections"), torch.sum(discretized_outputs == 1).float().item(), on_step=True, on_epoch=True)
         if torch.sum(discretized_outputs == 1).item() > 0:
             self.log(naming("precision"),
                      (torch.sum(torch.logical_and(discretized_outputs == 1, labels == 1)) /
@@ -101,33 +83,31 @@ class SupervisedDenseGrasp(pl.LightningModule):
         outputs = self.model(img)['out']
         return outputs
 
-    def step(self, batch, mode="train", batch_idx=0):
+    def step(self, batch, mode="train", batch_idx=0, dataloader_idx=0):
         img, labels, mask = batch
-        assert img.shape == (self.hparams.batch_size, 3, 224, 224)
-        assert labels.shape == (self.hparams.batch_size, 1, 224, 224)
-        assert mask.shape == (self.hparams.batch_size, 1, 224, 224)
+        '''
+        assert img.shape[1:] == (3, 224, 224), img.shape
+        assert labels.shape[1:] == (1, 224, 224), labels.shape
+        assert mask.shape[1:] == (1, 224, 224), mask.shape
+        '''
 
         outputs = self.forward(img)
         discretized_outputs = torch.as_tensor(torch.sigmoid(outputs) > 0.5, dtype=torch.long)
 
-        #loss = torch.nn.BCEWithLogitsLoss(
-        #    weight=((labels == 0) + (labels == 1) * self.hparams.positive_loss_scaling) * mask)(outputs, labels)
-        pos_weight = torch.ones(224, 224, device=outputs.device) * 62.
-        loss = torch.BCEWithLogitsLoss(pos_weight=pos_weight)
+        loss = torch.nn.BCEWithLogitsLoss(
+            weight=((labels == 0) + (labels == 1) * self.hparams.positive_loss_scaling) * mask)(outputs, labels)
+        #pos_weight = torch.ones(224, 224, device=outputs.device) * 62.
+        #loss = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
         for top_n in [None, 1, 5, 10, 20]:
             self.logging(mode, batch, outputs, loss, top_n)
 
-        predimg = T.ToPILImage()(discretized_outputs[0] * 1.0)
-        pimg = T.ToPILImage()(img[0])
-        plabels = T.ToPILImage()(labels[0])
-
-        logger.report_image(mode, "image_prediction", iteration=self.log_iter, image=predimg)
-        if mode == "val":
-            logger.report_image(mode, "img", iteration=self.log_iter, image=pimg)
-        logger.report_image(mode, "labels", iteration=self.log_iter, image=plabels)
-
-        self.log_iter += 1
+        if mode == "val" and batch_idx in [0, 1, 2]:
+            tensorboard = self.logger.experiment
+            #images = torch.stack((img[0], labels[0], discretized_outputs[0]))
+            tensorboard.add_image(f"{dataloader_idx}_{batch_idx}_{mode}_image", img[0], self.current_epoch)
+            tensorboard.add_image(f"{dataloader_idx}_{batch_idx}_{mode}_image_ground_truth", labels[0], self.current_epoch)
+            tensorboard.add_image(f"{dataloader_idx}_{batch_idx}_{mode}_image_prediction", discretized_outputs[0] * 1.0, self.current_epoch)
 
         return loss
 
@@ -135,9 +115,11 @@ class SupervisedDenseGrasp(pl.LightningModule):
         # finding counterexamples here
         img, labels, mask = batch
 
-        pimg = T.ToPILImage()(img[0])
-        plabels = T.ToPILImage()(labels[0])
-        logger.report_image("train", "img", iteration=self.log_iter, image=pimg)
+        if self.use_deepaug and random.random() < 0.6:
+            img = self.deepaug(img)
+        if batch_idx in [0, 1, 2]:
+            tensorboard = self.logger.experiment
+            tensorboard.add_image(f"train_{batch_idx}_image", img[0], self.current_epoch)
 
         if self.robust:
             self.eval()
@@ -156,8 +138,8 @@ class SupervisedDenseGrasp(pl.LightningModule):
             self.log("maxdif", pdifimg.max(), on_step=True, on_epoch=False)
             pimg = T.ToPILImage()(x_adv[0])
             pdifimg = T.ToPILImage()(pdifimg)
-            logger.report_image("train", "image_adv", iteration=self.log_iter, image=pimg)
-            logger.report_image("train", "pdifimg", iteration=self.log_iter, image=pdifimg)
+            #logger.report_image("train", "image_adv", iteration=self.log_iter, image=pimg)
+            #logger.report_image("train", "pdifimg", iteration=self.log_iter, image=pdifimg)
 
             self.train()
         #logger.report_image("image", "image_lab", iteration=batch_idx, image=plabels)
@@ -165,21 +147,24 @@ class SupervisedDenseGrasp(pl.LightningModule):
         self.bidx = batch_idx
         return self.step(batch, mode="train", batch_idx=batch_idx)
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx, dataloader_idx):
         self.bidx = batch_idx
-        return self.step(batch, mode="val", batch_idx=batch_idx)
+        return self.step(batch, mode="val", batch_idx=batch_idx, dataloader_idx=dataloader_idx)
 
 
-def train_supervised(datamodule, model=None, **kwargs):
+
+def train_supervised(train_loaders, val_loaders, model=None, **kwargs):
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
     def train(**kwargs):
         trainer = pl.Trainer(
             default_root_dir="models/",
             gpus=1 if str(device) == "cuda:0" else 0,
+            #gpus=-1,
             max_epochs=kwargs["max_epochs"],
             callbacks=[
-                ModelCheckpoint(save_weights_only=True, mode="min", monitor="val_loss"),
+                #ModelCheckpoint(save_weights_only=True, mode="min", monitor="0_val_loss/dataloader_idx_0"),
+                #ModelCheckpoint(save_weights_only=True, mode="min", monitor="0_val_loss/dataloader_idx_1"),
                 LearningRateMonitor("step"),
             ],
             progress_bar_refresh_rate=1,
@@ -188,49 +173,50 @@ def train_supervised(datamodule, model=None, **kwargs):
 
         nonlocal model
         model = model if model else SupervisedDenseGrasp(**kwargs)
-        trainer.fit(model, datamodule)
+        trainer.fit(model, train_dataloaders=train_loaders, val_dataloaders=val_loaders)
         return model
 
     return train(**kwargs)
 
 
 if __name__ == "__main__":
-    #change this
-    test_on_augmented = True#False#True
-    robust = True#False#True
+    # hyperparameters (change this if you need)
+    # robust
+    robust = False
     iterations = 30
-    #eps = 0.05
     eps = 50.
     step_size = 0.01
-    step_type="l2"
+    step_type = "l2"
 
-    task_name = f"augmented: {test_on_augmented}, robust: {robust}, steps: {iterations}, eps: {eps} step_size: {step_size}, step_type: {step_type}"
+    # other
+    batch_size = 8
+    task_name = "grip_dense"
+    dataset_path = "/home/rm360179/datasets/grasping_dataset"
+    use_deepaug = True
 
-    ###
+    task = Task.init(project_name="robustness", task_name=task_name, reuse_last_task_id=False)
 
+    train = DenseGraspDatasetRobust(450, directory=dataset_path)
+    val = DenseGraspDatasetRobust(50, 450, directory=dataset_path)
+    val_augmented = DenseGraspDatasetRobustAugmented(50 * 10, 4500, directory=dataset_path)
 
-    if test_on_augmented:
-        train, test = DenseGraspDatasetRobust(450, directory="robust_dense"), DenseGraspDatasetRobustAugmented(50 * 10, 4500, directory="robust_dense")
-        datamodule = DenseGraspDataModule(train, test, batch_size=8)
-        task = Task.init(project_name="robustness", task_name=task_name, reuse_last_task_id=False)
-    else:
-        train, test = DenseGraspDatasetRobust(450, directory="robust_dense"), DenseGraspDatasetRobust(50, 450, directory="robust_dense")
-        datamodule = DenseGraspDataModule(train, test, batch_size=8)
-        task = Task.init(project_name="robustness", task_name=task_name, reuse_last_task_id=False)
-    logger = task.get_logger()
-    img_log_step = 0
+    train_loader = torch.utils.data.DataLoader(train, batch_size)
+    val_loader = torch.utils.data.DataLoader(val, batch_size)
+    val_augmented_loader = torch.utils.data.DataLoader(val_augmented, batch_size)
 
     train_supervised(
-        datamodule,
+        train_loader,
+        [val_loader, val_augmented_loader],
         model=None,
-        batch_size=datamodule.batch_size,
+        batch_size=batch_size,
         lr=1e-3,
         dropout=0.01,
         weight_decay=0.01,
-        max_epochs=90,
+        max_epochs=300,
         robust=robust,
         iterations=iterations,
         eps=eps,
         step_size=step_size,
-        step_type=step_type
+        step_type=step_type,
+        use_deepaug=use_deepaug
     )
